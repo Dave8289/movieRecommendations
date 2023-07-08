d7024717927c439af62f54e43243bf781f73180f
Davinder Kumar <davindertwal@gmail.com>
2023-07-08 15:48:29 +0530

    final

---
+++ src/components/AddMovie.js
@@ -0,0 +1,44 @@
+import React, { useRef } from "react";
+
+import classes from "./AddMovie.module.css";
+
+function AddMovie(props) {
+  const titleRef = useRef("");
+  const openingTextRef = useRef("");
+  const releaseDateRef = useRef("");
+
+  function submitHandler(event) {
+    event.preventDefault();
+
+    // could add validation here...
+
+    const movie = {
+      title: titleRef.current.value,
+      openingText: openingTextRef.current.value,
+      releaseDate: releaseDateRef.current.value,
+    };
+
+    props.onAddMovie(movie);
+  }
+
+  return (
+    <form onSubmit={submitHandler}>
+      <h1>My Personal Movie Recommendations</h1>
+      <div className={classes.control}>
+        <label htmlFor="title">Movie Name</label>
+        <input type="text" id="title" ref={titleRef} />
+      </div>
+      <div className={classes.control}>
+        <label htmlFor="opening-text">Review</label>
+        <textarea rows="5" id="opening-text" ref={openingTextRef}></textarea>
+      </div>
+      <div className={classes.control}>
+        <label htmlFor="date">Watch Date</label>
+        <input type="text" id="date" ref={releaseDateRef} />
+      </div>
+      <button>Add Movie</button>
+    </form>
+  );
+}
+
+export default AddMovie;
